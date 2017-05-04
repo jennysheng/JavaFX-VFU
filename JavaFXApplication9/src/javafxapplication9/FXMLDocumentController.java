e0f7982bd7525fe0e7705674387b19fd65135929
jennysheng <qd_sheng2000@hotmail.com>
2017-05-04 17:48:22 +0200

    creatchart

--- JavaFXApplication9/src/javafxapplication9/FXMLDocumentController.java
+++ JavaFXApplication9/src/javafxapplication9/FXMLDocumentController.java
@@ -10,6 +10,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -17,6 +20,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ListView;
@@ -29,25 +33,31 @@ import javafx.stage.FileChooser;
 public class FXMLDocumentController implements Initializable {
 
     @FXML
-    private ScatterChart<?, ?> scatterchart;
+    private ArrayList<ScatterChart.Series> scatterchartseries = new ArrayList<>();
 
     private final ListView<Double> channel1 = new ListView<Double>();
 
     private final ObservableList<DataLogger> channelsdata = FXCollections.observableArrayList();
+
+    ScatterChart.Series series1, series2, series3, series4, series5, series6, series7, series8;
+
     @FXML
     private Button buttonChannel1;
     @FXML
     private ColorPicker colorPicker1;
 
     File selectedFile = null;
+    int f = 0;
+    JavaFXApplication9 aThis;
 
     @FXML
     private void handleReadAction(ActionEvent event) throws InterruptedException {
 
         FileChooser fileChooser = new FileChooser();
         selectedFile = fileChooser.showOpenDialog(null);
-        Thread.sleep(1000);
-        addSenorValueFromFile();
+     
+
+        createChart();
 
     }
 
@@ -82,13 +92,10 @@ public class FXMLDocumentController implements Initializable {
                 System.out.println(j);
                 int i = 0;
 
-                while (i < ((j-1) * 9)) {
+                while (i < ((j - 1) * 9)) {
 
                     channelsdata.add(new DataLogger(parts[i], parts[i + 1], parts[i + 2], parts[i + 3], parts[i + 4], parts[i + 5], parts[i + 6], parts[i + 7], parts[i + 8]));
                     i += 9;
-                    
-                   
-
                 }
 
             } catch (IOException e) {
@@ -98,12 +105,36 @@ public class FXMLDocumentController implements Initializable {
         return channelsdata;
     }
 
-    public ObservableList<DataLogger> getChannelsdata() {
-        return channelsdata;
+    public void createChart() {
+
+        series1 = new ScatterChart.Series();
+        series2 = new ScatterChart.Series();
+        series3 = new ScatterChart.Series();
+        series4 = new ScatterChart.Series();
+        series5 = new ScatterChart.Series();
+        series6 = new ScatterChart.Series();
+        series7 = new ScatterChart.Series();
+        series8 = new ScatterChart.Series();
+
+        SimpleDateFormat date = new SimpleDateFormat("yyy-MM-dd:HH:mm:ss,SSS");
+
+        this.addSenorValueFromFile().stream().forEach((c) -> {
+
+            series1.getData().add(new ScatterChart.Data(f, Double.parseDouble(c.getValue1())));
+            series2.getData().add(new ScatterChart.Data(f, Double.parseDouble(c.getValue2())));
+            series3.getData().add(new ScatterChart.Data(f, Double.parseDouble(c.getValue3())));
+            series4.getData().add(new XYChart.Data(f, Double.parseDouble(c.getValue4())));
+            series5.getData().add(new XYChart.Data(f, Double.parseDouble(c.getValue5())));
+            series6.getData().add(new XYChart.Data(f, Double.parseDouble(c.getValue6())));
+            series7.getData().add(new XYChart.Data(f, Double.parseDouble(c.getValue7())));
+            series8.getData().add(new XYChart.Data(f, Double.parseDouble(c.getValue8())));
+            f++;
+
+        });
+        scatterchartseries.addAll(Arrays.asList(series1, series2, series3, series4, series5, series6, series7, series8));
+
     }
 
-    private void plotChart() {
-
-    }
+    
 
 }
